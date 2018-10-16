c34f5b0b97dcb9db9b11a5c05c54b0f2540265bb
Maceo <mlewisjr89@gmail.com>
2018-10-16 14:34:55 -0400

    created a reusable components

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
-const Header = () => {
+const Header = (props) => {
+    console.log(props)
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Maceo
+                {props.playerName}
             </span>
 
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <button className="counter-action increment"> + </button>
-                <span className="counter-score">35</span>
-            </div>
+            <Counter playerScore={props.playerScore} />
+        </div>
+    );
+}
+
+const Counter  = (props) => {
+    return(
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>           
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header title="scoreboard" totalPlayers={1}/>
+
+            {/*Players list*/}
+            <Player playerName="Maceo" playerScore={100}/>
         </div>
     );
 }
 
 ReactDOM.render(
-    <Player />,
+    <App />,
     document.getElementById('root')
 );
