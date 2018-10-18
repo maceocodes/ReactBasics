df3cef509dc9a4f03382cdd5aa84d749d8dd5032
Maceo Lewis <maceo.code@gmail.com>
2018-10-17 21:51:36 -0400

    starting on players array itteration

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const players = [
+    {
+        name: "Guil",
+        score: 50
+      },
+      {
+        name: "Treasure",
+        score: 85
+      },
+      {
+        name: "Ashley",
+        score: 95
+      },
+      {
+        name: "James",
+        score: 80
+      }
+];
+
+
 const Header = (props) => {
     console.log(props)
     return (
@@ -24,24 +44,29 @@ const Counter  = (props) => {
     return(
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.playerScore}</span>
             <button className="counter-action increment"> + </button>           
         </div>
     );
 }
 
-const App = () => {
+const App = (props) => {
     return (
         <div className="scoreboard">
             <Header title="scoreboard" totalPlayers={1}/>
 
             {/*Players list*/}
-            <Player playerName="Maceo" playerScore={100}/>
+            {props.initalPlayers.map}
+            <Player playerName="Maceo" playerScore={100} />
+            <Player playerName="Player01" playerScore={70} />
+            <Player playerName="Player02" playerScore={67} />
+            <Player playerName="Player03" playerScore={53} />
+            
         </div>
     );
 }
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={players}/>,
     document.getElementById('root')
 );
